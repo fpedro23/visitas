2e7d9b5f0293586832233bee2816b04b63448dae
Pedro Contreras <fpedro23@gmail.com>
2015-10-27 13:32:09 -0600

    BD DATA

--- visitas/settings.py
+++ visitas/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'visitas',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'visitas',
+        'PASSWORD': 'visitas',
         'HOST': '',
         'PORT': '',
     }
