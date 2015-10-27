544300e912485c20a82d9e9ea43f9422d40d762d
Pedro Contreras <fpedro23@gmail.com>
2015-10-27 14:09:40 -0600

    STATIC URL

--- visitas/settings.py
+++ visitas/settings.py
@@ -140,6 +140,10 @@ TEMPLATETAGS_DIRS = (
     os.path.join(BASE_DIR, 'visitas_stg/templatetags/'),
 )
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/home/sisefenlin/visitas/static/',
+)
 
 SESSION_COOKIE_AGE = 35900
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
