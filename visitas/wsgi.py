16e4b80cbbbede742e17aef1222cbab937320344
Pedro Contreras <fpedro23@gmail.com>
2015-10-27 11:22:02 -0600

    WSGI.PY

--- visitas/wsgi.py
+++ visitas/wsgi.py
@@ -6,9 +6,26 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
 import os
+import sys
+import site
+
+os.environ['PYTHON_EGG_CACHE'] = '/eggCache'
+
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/sisefenlin/visitas/ve/lib/python2.7/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/sisefenlin/visitas')
+sys.path.append('/home/sisefenlin/visitas/visitas_stg')
+
+# Activate your virtual env
+activate_env = os.path.expanduser("/home/sisefenlin/visitas/ve/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "visitas.settings")
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
